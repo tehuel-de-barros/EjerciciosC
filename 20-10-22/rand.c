236227f64bb9113f85ef191d44692f0c41f012c2
tehuel-de-barros <tehueldebarros@gmail.com>
2022-10-20 10:36:28 -0300

    segundo commit

--- 20-10-22/rand.c
+++ 20-10-22/rand.c
@@ -3,7 +3,7 @@
 int main(int argc, char const *argv[]) {
   int n = atoi(argv[1]);
   for (int i = 0; i < n; i++) {
-    printf("%d\n",rand() % 5);
+    printf("%d\n",rand() % 15);
   }
   return 0;
 }
